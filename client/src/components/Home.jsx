b9dae03b12100ab0a3a06d96bee71314b24ad074
Ashwin Nair <hashwin381@gmail.com>
2022-11-30 22:44:34 +0530

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,88 +1,44 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
 import {Header} from "./index";
 import {useStateValue} from "../context/StateProvider";
-// import Filter from "./Filter";
+
 import {getAllSongs} from "../API";
 import {actionType} from "../context/reducer";
-import {motion} from "framer-motion";
-import {filters} from "../utilities/supportfunctions";
-
+import {SongContainer} from "./DashboardSongs";
 
 
 const Home = () => {
 
-    const [{allSongs}, dispatch] = useStateValue();
+    const [{allSongs}] = useStateValue();
 
     const HomeSongContainer = ({ musics }) => {
         const [{ isSongPlaying, songIndex }, dispatch] = useStateValue();
 
-        const addSongToContext = (index) => {
-            if (!isSongPlaying) {
-                dispatch({
-                    type: actionType.SET_IsSONG_PLAYING,
-                    isSongPlaying: true,
+        useEffect(() => {
+            if (!allSongs) {
+                getAllSongs().then((data) => {
+                    dispatch({
+                        type: actionType.SET_ALL_SONGS,
+                        allSongs: data.song,
+                    });
                 });
             }
-            if (songIndex !== index) {
-                dispatch({
-                    type: actionType.SET_SONG_INDEX,
-                    songIndex: index,
-                });
-            }
-        };
+        }, []);
 
         return (
-            <>
-                {musics?.map((data, index) => (
-                    <motion.div
-                        key={data._id}
-                        whileTap={{ scale: 0.8 }}
-                        initial={{ opacity: 0, translateX: -50 }}
-                        animate={{ opacity: 1, translateX: 0 }}
-                        transition={{ duration: 0.3, delay: index * 0.1 }}
-                        className="relative w-40 min-w-210 px-2 py-4 cursor-pointer hover:shadow-xl hover:bg-card bg-gray-100 shadow-md rounded-lg flex flex-col items-center"
-                        onClick={() => addSongToContext(index)}
-                    >
-                        <div className="w-40 min-w-[160px] h-40 min-h-[160px] rounded-lg drop-shadow-lg relative overflow-hidden">
-                            <motion.img
-                                whileHover={{ scale: 1.05 }}
-                                src={data.imageURL}
-                                alt=""
-                                className=" w-full h-full rounded-lg object-cover"
-                            />
-                        </div>
 
-                        <p className="text-base text-headingColor font-semibold my-2">
-                            {data.name.length > 25 ? `${data.name.slice(0, 25)}` : data.name}
-                            <span className="block text-sm text-gray-400 my-1">
-                            {data.artist}
-                            </span>
-                        </p>
-                    </motion.div>
-                ))}
-
-            </>
+            <div className='w-full p-4 flex items-center justify-center flex-col'>
+                <div className="relative w-full  my-4 p-4 py-12  border-gray-300 rounded-md">
+                    <SongContainer data={allSongs} />
+                </div>
+            </div>
         );
     };
-
-
-    useEffect(() => {
-        if (!allSongs) {
-            getAllSongs().then((data) => {
-                dispatch({
-                    type: actionType.SET_ALL_SONGS,
-                    allSongs: data.song,
-                });
-            });
-        }
-    }, []);
-
 
     return(
         <div className='w-full h-auto flex flex-col items-center justify-center bg-zinc-900'>
         <Header/>
             <h1 className=" text-3xl text-white">Discover New Songs </h1>
-            <br/><br/>
         <div className="w-full h-auto flex items-center justify-evenly gap-4 flex-wrap p-4 bg-zinc-900'">
             <HomeSongContainer musics={allSongs} />
         </div>
